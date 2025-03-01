9567a47015458d2c7bb199fc6d1356f253ec6cee
Qman11010101 <30486982+Qman11010101@users.noreply.github.com>
2025-03-01 22:39:44 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 window.addEventListener("message", (event) => {
     console.log(event.origin)
+    console.log(event.data)
     if (event.origin !== "https://chunithm-net-eng.com" || event.origin !== "https://new.chunithm-net.com") return
     
     const textarea = document.querySelector("ta-posted")
