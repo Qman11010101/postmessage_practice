bab64f430c60791f7d3c739d20c9dd5f74cf831f
Qman11010101 <30486982+Qman11010101@users.noreply.github.com>
2025-03-02 01:45:12 +0900

    Update main.js

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 window.addEventListener("message", (event) => {
+    console.log(event.origin)
     if (event.origin !== "https://chunithm-net-eng.com" || event.origin !== "https://new.chunithm-net.com") return
-    console.log(event)
     const textarea = document.querySelector("ta-posted")
     textarea.value = event.data
 });
