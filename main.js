a20d2a4bc5ef13a51a0cc7ababb6aa0776a78306
Qman11010101 <30486982+Qman11010101@users.noreply.github.com>
2025-03-02 00:05:32 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 window.addEventListener("message", (event) => {
+    console.log("ORIGIN: " + event.origin)
+    console.log("REFERRER: " + document.referrer)
     if (event.origin !== "https://chunithm-net-eng.com" || event.origin !== "https://new.chunithm-net.com") return
     console.log(event.origin)
     console.log(document.referrer)
